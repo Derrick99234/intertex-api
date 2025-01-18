0249bf487d3025daa737b36f12a74c7001279ffc
Derrick <pshubomi@gmail.com>
2025-01-18 16:54:30 +0100

    done with authentication

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,31 +1,83 @@
 import { Request, Response } from "express";
 import User from "../models/auth.model";
 import * as bcryptjs from "bcryptjs";
+import { generateToken } from "../utils/helper/jwt-process.helper";
 
-export const createUser = async (request: Request, response: Response) => {
+export async function createUser(
+  request: Request,
+  response: Response
+): Promise<any> {
   const { username, password, email, firstName, lastName } = request.body;
-
   try {
-    const hashPassword = bcryptjs.hashSync(password, 10);
+    if (!username || !password || !email || !firstName || !lastName) {
+      return response.status(400).json({ message: "All fields are required" });
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
     const user = new User({
       username,
-      password: hashPassword,
+      password: hashedPassword,
       email,
       firstName,
       lastName,
     });
-    await user.save();
-    response.status(201).json({ message: "User created successfully", user });
+    const payload = {
+      id: String(user._id),
+      email,
+    };
+    const token = generateToken(payload);
+    response
+      .status(201)
+      .json({ message: "User created successfully", user, token });
   } catch (error) {
     console.error(error);
     response.status(500).json({ message: "Error creating user", error });
   }
-};
+}
 
-export function login(request: Request, response: Response) {
-  const { username, password } = request.body;
-  response.status(200).json({
-    username,
-    password,
-  });
+export async function login(
+  request: Request,
+  response: Response
+): Promise<any> {
+  try {
+    const { username, password } = request.body;
+
+    if (!username || !password) {
+      return response.status(400).jsonp();
+    }
+
+    const user = await User.findOne({
+      $or: [{ username }, { email: username }],
+    });
+
+    if (!user) {
+      return response.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+
+    if (!isMatch) {
+      return response.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const payload = {
+      id: String(user._id),
+      email: user.email,
+    };
+    const token = generateToken(payload);
+
+    response.status(200).json({
+      message: "Login successful",
+      user,
+      token,
+    });
+
+    response.status(200).json({
+      username,
+      password,
+    });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Error logining user", error });
+  }
 }
---
+++ middlewares/authMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/helper/jwt-process.helper";
+import { UserRequest } from "../utils/interface/types";
+
+export const authenticate = (
+  req: UserRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer token
+
+  if (!token) return res.status(401).json({ message: "No token provided" });
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};

a00a7b54bb3815c166b832ceb3928dedf2bdf37e
Derrick <pshubomi@gmail.com>
2025-01-20 00:36:26 +0100

    made necessary changes that ensures authentication works

--- utils/helper/jwt-process.helper.ts
+++ utils/helper/jwt-process.helper.ts
@@ -1,5 +1,7 @@
 import * as jwt from "jsonwebtoken";
 import { Payload } from "../interface/types";
+import dotenv from "dotenv";
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
